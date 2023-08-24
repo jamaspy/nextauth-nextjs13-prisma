eb82b51ae8febc918e01318768a63d23acfb9965
jamaspy <aspin@me.com>
2023-08-24 19:32:53 +1000

    revert middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ export default async function Home() {
       <Header />
       <section className="bg-ct-blue-600 min-h-screen pt-20">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-          <p className="text-3xl font-semibold">
-            Welcome to NextAuth with Prisma ORM and Next.js 13
-          </p>
+          <p className="text-3xl font-semibold">Escape Room Admin Center</p>
         </div>
       </section>
     </>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // matcher: ["/profile"],
-  matcher: ["/((?!register|api|login).*)", "/profile"],
+  matcher: ["/profile", "/register"],
+  // matcher: ["/((?!register|api|login).*)"],
 };
